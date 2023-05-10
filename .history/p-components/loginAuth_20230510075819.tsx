1be3e809abfc61e5fd6cded880c5dacef845c0d5
Yahya9102 <112933451+Yahya9102@users.noreply.github.com>
2023-05-10 07:58:30 +0200

    fixed loginAuth sessionstorage

---
+++ .history/p-components/loginAuth_20230510075819.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+const loginAuth = (WrappedComponent: NextPage<any>) => {
+  const AuthenticatedComponent: React.FC = (props) => {
+    const router = useRouter()
+    const isAuthenticated =
+      typeof sessionStorage !== "undefined" && sessionStorage.getItem("email") //check this later
+
+    useEffect(() => {
+      if (!isAuthenticated) {
+        router.push("/loginPage")
+      }
+    }, [])
+
+    return isAuthenticated ? <WrappedComponent {...props} /> : null
+  }
+
+  return AuthenticatedComponent
+}
+
+export default loginAuth
